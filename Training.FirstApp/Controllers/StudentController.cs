dc2c5c0ba516a8e31d83b79f97cbf7cf3ec01e61
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-03-29 19:30:28 +0530

    session 5

--- Training.FirstApp/Controllers/StudentController.cs
+++ Training.FirstApp/Controllers/StudentController.cs
@@ -32,5 +32,35 @@ namespace Training.FirstApp.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Student student)
+        {
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Training.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Training.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
